f186e436655d5296584a10b7789a5db9a2bf6082
Graeme-Dunlop <graemedunlop1983@gmail.com>
2022-04-07 09:22:47 +0100

    some css

--- src/components/Song.js
+++ src/components/Song.js
@@ -5,15 +5,20 @@ const Song = ({song, index}) => {
     return (
         <li className="list-item"> 
             <div className="list-item-left">
-                <h1>Chart # {index+1}</h1> 
-                <h2>Song Title: {song['im:name'].label} </h2> 
-                <h3>Artist: {song['im:artist'].label}</h3> 
-                <audio controls src={song.link[1].attributes.href} />
+                <div className="list-item-left-left">
+                    <img src={song['im:image'][2].label} />
+                </div>
+                <div className="list-item-left-right">
+                    <h1>Chart # {index+1}</h1> 
+                    <h2>Song Title: {song['im:name'].label} </h2> 
+                    <h3>Artist: {song['im:artist'].label}</h3> 
+                    <audio className="audio-bar" controls src={song.link[1].attributes.href} />
+                </div>
             </div>
-            <div className="list-item-right">
-                <img src={song['im:image'][2].label} />
+            
                 
-            </div>
+                
+            
         </li>
     )
 }
